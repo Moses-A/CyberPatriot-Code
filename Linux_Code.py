452b09d9efcba0d4b556a225cb16b1607b4eb144
Moses Arocha <carocha24@gmail.com>
2016-09-05 18:51:05 -0500

    Update

--- Linux_Code.py
+++ Linux_Code.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python2
 # written by Moses Arocha
 
+
 import os
 from Tkinter import *
 import tkMessageBox
 import json
+
 
 window = Tk()
 window.title("CyberPatriot Code")
 window.geometry("350x50")
 window.configure(background="#a1bdcd")
 
+
 def getIP():
     print ipVar
+
 
 attack = Label(window, text=" Please Check Attacking IP ADDRESS Before Attacks")
 enter = Button(window, text = "Check IP", command = getIP)
@@ -23,155 +27,167 @@ ipVar = raw_input(" Please Insert IP Address: ")
 attack.pack()
 enter.pack()
 
+
 def Tshark():
-   filewin = Toplevel(window)
-   button = Button(filewin, text="Verison Of Tshark")
-   RunTshark = os.system('tshark -v')
-   print RunTshark  
-   button.pack()
+    filewin = Toplevel(window)
+    button = Button(filewin, text="Verison Of Tshark")
+    RunTshark = os.system('tshark -v')
+    print RunTshark  
+    button.pack()
+
 
 def TsharkInstall():
-   filewin = Toplevel(window)
-   button = Button(filewin, text="Installing Tshark")
-   InstallTShark = os.system('sudo apt-get update | sudo apt-get upgrade | sudo apt-get install tshark')
-   print InstallTShark
-   button.pack()
+    filewin = Toplevel(window)
+    button = Button(filewin, text="Installing Tshark")
+    InstallTShark = os.system('sudo apt-get update | sudo apt-get upgrade | sudo apt-get install tshark')
+    print InstallTShark
+    button.pack()
+
 
 def TsharkSetup():
-   filewin = Toplevel(window)
-   button = Button(filewin, text="If Tshark doesn't Work")
-   FixTShark = os.system('sudo dpkg-reconfigure wireshark-common | sudo adduser wireshark')
-   print FixTShark
-   button.pack()
+    filewin = Toplevel(window)
+    button = Button(filewin, text="If Tshark doesn't Work")
+    FixTShark = os.system('sudo dpkg-reconfigure wireshark-common | sudo adduser wireshark')
+    print FixTShark
+    button.pack()
+
 
 def help():
-   filewin = Toplevel(window)
-   button = Button(filewin, text="This code is written in Python and uses the Terminal as output. In order to get the full effect please run this code as a SuperUser.")
-   button.pack()
+    filewin = Toplevel(window)
+    button = Button(filewin, text="This code is written in Python and uses the Terminal as output. In order to get the full effect please run this code as a SuperUser.")
+    button.pack()
+
 
 def about():
-   filewin = Toplevel(window)
-   button = Button(filewin, text="This is a program, written by Moses Arocha which utilizes the tools of Nmap and Tshark for the Holmes High School CyberPatriot Team. All Rights Reserverd.")
-   button.pack()
+    filewin = Toplevel(window)
+    button = Button(filewin, text="This is a program, written by Moses Arocha which utilizes the tools of Nmap and Tshark for the Holmes High School CyberPatriot Team. All Rights Reserverd.")
+    button.pack()
+
 
 def TsharkNetworkAdapter():
-   filewin = Toplevel(window)
-   button = Button(filewin, text=" Should State: 1. eth0 2. nflog 3.nfqueue 4. any 5.lo")
-   NetworkAdapter = os.system('sudo tshark -D')
-   print NetworkAdapter
-   button.pack()
+    filewin = Toplevel(window)
+    button = Button(filewin, text=" Should State: 1. eth0 2. nflog 3.nfqueue 4. any 5.lo")
+    NetworkAdapter = os.system('sudo tshark -D')
+    print NetworkAdapter
+    button.pack()
+
 
 def TsharkAny():
-   filewin = Toplevel(window)
-   button = Button(filewin, text=" Monitoring Any Network...")
-   AnyMonitor = os.system('sudo tshark -i any')
-   print AnyMonitor
-   button.pack()
+    filewin = Toplevel(window)
+    button = Button(filewin, text=" Monitoring Any Network...")
+    AnyMonitor = os.system('sudo tshark -i any')
+    print AnyMonitor
+    button.pack()
+
 
 def TsharkEth():
-   filewin = Toplevel(window)
-   button = Button(filewin, text=" Monitoring Ethernet Only...")
-   EthMonitor = os.system('sudo tshark -i -eth0')
-   print EthMonitor
-   button.pack()
+    filewin = Toplevel(window)
+    button = Button(filewin, text=" Monitoring Ethernet Only...")
+    EthMonitor = os.system('sudo tshark -i -eth0')
+    print EthMonitor
+    button.pack()
+
 
 def TsharkSpecs():
-   filewin =Toplevel(window)
-   button = Button(filewin, text=" Package Number, Time Stamp, Sender's IP Address, Receiver's IP Address, Package Contents")
-   Specification = os.system('sudo tshark -i any -T fields -e frame.number -e frame.time_relative -e ip.src -e ip.dst -e col.Info')
-   print Specification
-   button.pack()
+    filewin =Toplevel(window)
+    button = Button(filewin, text=" Package Number, Time Stamp, Sender's IP Address, Receiver's IP Address, Package Contents")
+    Specification = os.system('sudo tshark -i any -T fields -e frame.number -e frame.time_relative -e ip.src -e ip.dst -e col.Info')
+    print Specification
+    button.pack()
+
 
 # beginning of code for Nmap
 
 def Nmap():
-   ipstring="sudo nmap -v "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="Ping, SYN Stealth Scan, TCP Port Scan")
-   Ping = os.system(ipstring)
-   print Ping
-   button.pack()
+    ipstring="sudo nmap -v "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="Ping, SYN Stealth Scan, TCP Port Scan")
+    Ping = os.system(ipstring)
+    print Ping
+    button.pack()
+
 
 def NmapPing():
-   ipstring="sudo nmap -sP "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="Ping Scan")
-   Ping = os.system(ipstring)
-   print Ping
-   button.pack()
+    ipstring="sudo nmap -sP "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="Ping Scan")
+    Ping = os.system(ipstring)
+    print Ping
+    button.pack()
+
 
 def NmapSyn():
-   ipstring="sudo nmap -sS "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="Syn Scan")
-   Syn = os.system(ipstring)
-   print Syn
-   button.pack()
+    ipstring="sudo nmap -sS "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="Syn Scan")
+    Syn = os.system(ipstring)
+    print Syn
+    button.pack()
 
 def NmapUDP():
-   ipstring="sudo nmap -sU "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="UDP Port Scan")
-   UDP = os.system(ipstring)
-   print UDP
-   button.pack()
+    ipstring="sudo nmap -sU "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="UDP Port Scan")
+    UDP = os.system(ipstring)
+    print UDP
+    button.pack()
 
 def NmapPort():
-   ipstring="sudo nmap -p1-65535 "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="Full Port Scan")
-   FullPort = os.system(ipstring)
-   print FullPort
-   button.pack()
-
+    ipstring="sudo nmap -p1-65535 "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="Full Port Scan")
+    FullPort = os.system(ipstring)
+    print FullPort
+    button.pack()
+  
 def NmapVersion():
-   ipstring="sudo nmap -sV "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="Version Detection")
-   Verison = os.system(ipstring)
-   print Verison
-   button.pack()
+    ipstring="sudo nmap -sV "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="Version Detection")
+    Verison = os.system(ipstring)
+    print Verison
+    button.pack()
 
 def NmapOS():
-   ipstring="sudo nmap -O "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="OS Detection")
-   OSD = os.system(ipstring)
-   print OSD
-   button.pack()
+    ipstring="sudo nmap -O "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="OS Detection")
+    OSD = os.system(ipstring)
+    print OSD
+    button.pack()
 
 def NmapIPv6():
-   ipstring="sudo nmap -6 "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="IPv6 Scan")
-   IPv6 = os.system(ipstring)
-   print IPv6
-   button.pack()
-
+    ipstring="sudo nmap -6 "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="IPv6 Scan")
+    IPv6 = os.system(ipstring)
+    print IPv6
+    button.pack()
+ 
 def NmapSpoofing():
-   macadd = raw_input(" Please Enter Attacking MAC ADDRESS: ")
-   ipstring="sudo nmap --spoof-mac "+macadd
-   filewin =Toplevel(window)
-   button = Button(filewin, text="Ping, SYN Stealth Scan, TCP Port Scan")
-   macattack = os.system(ipstring)
-   print macattack
-   button.pack()
+    macadd = raw_input(" Please Enter Attacking MAC ADDRESS: ")
+    ipstring="sudo nmap --spoof-mac "+macadd
+    filewin =Toplevel(window)
+    button = Button(filewin, text="Ping, SYN Stealth Scan, TCP Port Scan")
+    macattack = os.system(ipstring)
+    print macattack
+    button.pack()
 
 def NmapAggressive():
-   ipstring="sudo nmap -A "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="Aggressive Scan")
-   Aggressive = os.system(ipstring)
-   print Aggressive
-   button.pack()
+    ipstring="sudo nmap -A "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="Aggressive Scan")
+    Aggressive = os.system(ipstring)
+    print Aggressive
+    button.pack()
 
 def NmapConnection():
-   ipstring="sudo nmap -sT "+ipVar
-   filewin =Toplevel(window)
-   button = Button(filewin, text="Connection Detection")
-   Connect = os.system(ipstring)
-   print Connect
-   button.pack()
+    ipstring="sudo nmap -sT "+ipVar
+    filewin =Toplevel(window)
+    button = Button(filewin, text="Connection Detection")
+    Connect = os.system(ipstring)
+    print Connect
+    button.pack()
    
 menubar = Menu(window)
 tsharkmenu = Menu(menubar, tearoff=0)
@@ -211,7 +227,6 @@ helpmenu.add_command(label="About...", command=about)
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 window.config(menu=menubar)
-
 
 
 window.mainloop()
